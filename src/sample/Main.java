6e1cd34dcdfe573079318972d965d13545efa948
fafa <fatemehveysi1378@gmail.com>
2019-05-17 13:13:01 -0700

    g

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
+//:
 
 
     public static void main(String[] args) {
